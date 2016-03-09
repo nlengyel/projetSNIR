b1a0b50cdf0de9aad98348d2bc1b7e983d3fbebf
nlengyel <nicolas.lengyel@gmail.com>
2016-03-09 18:25:48 +0100

    classe TransmissionTrames sans IHM ni scenario

--- TestTrame/main.cpp
+++ TestTrame/main.cpp
@@ -5,22 +5,21 @@
 #include <cstdlib>
 #include <stdio.h>
 #include <string>
+#include "gestionTrames.h"
 
 
 using namespace std ;
 
 
+
 int main( int argc,char** argv  )
 {
-char  data[]={"00128B550100242913000000000010E00004D1"};
-int checksum=0;
-for(int i=0;i<40;i++){
-    int recep=data[i];
-    checksum+=0x80 ^ recep;
-    //checksum+=checksum;
+    SerialPort *toto = new SerialPort("/dev/ttyUSB0");
+    gestionTrames *elem1= new gestionTrames(toto);
+    elem1->configuration();
+    elem1->test();
+
+return 0;
 }
-    cout<<checksum;
-    return 0;
-}
-    
+
     
